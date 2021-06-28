6b90f94d2a9bfedb9114e82d6fe6991a0892202a
Jadit19 <aditj20@iitk.ac.in>
2021-06-29 00:48:18 +0530

    Add Side Messages

--- app.js
+++ app.js
@@ -49,3 +49,54 @@ searchLabel.addEventListener('click', () => {
         search.style.left = "calc(50% - 60px)";
     }
 });
+
+//=====================================================================
+
+var i = 0;
+var letters = document.getElementsByClassName('falling-letters');
+var message = document.getElementById('messages');
+var messageBtn = document.getElementById('message_click');
+var redLine = document.getElementsByClassName('red-line');
+var me = document.getElementById('me');
+var h21 = document.getElementById('h21');
+var ele = document.getElementsByClassName('inside-div');
+var notif = document.getElementById('notif');
+messageBtn.addEventListener('click', () => {
+    if (message.style.right == "-400px"){
+        message.style.right = "0px";
+        redLine[0].style.animation = "sliding1 1.5s forwards";
+        redLine[1].style.animation = "sliding2 1.3s forwards";
+        redLine[0].style.animationDelay = "0.5s";
+        redLine[1].style.animationDelay = "1.5s";
+        me.style.animation = "falling 2s cubic-bezier(0.68, -0.55, 0.265, 1.55) forwards";
+        me.style.animationDelay = "1.5s";
+        h21.style.animation = "falling 1s cubic-bezier(0.68, -0.55, 0.265, 1.55) forwards"
+        h21.style.animationDelay = "2.5s";
+        notif.innerHTML = '';
+        notif.style.width = "0px";
+        notif.style.height = "0px";
+        for (i=0; i<16; i++){
+            letters[i].style.animation = "falling 1s cubic-bezier(0.68, -0.55, 0.265, 1.55) forwards";
+            letters[i].style.animationDelay = i/10 + 's';
+        }
+        for (i=0; i<5; i++){
+            ele[i].style.animation = "falling 1s cubic-bezier(0.68, -0.55, 0.265, 1.55) forwards"
+            ele[i].style.animationDelay = 3 + i/5 + "s";
+        }
+    } else {
+        message.style.right = "-400px";
+        redLine[0].style.animation = "none";
+        redLine[1].style.animation = "none";
+        me.style.animation = "none";
+        h21.style.animation = "none";
+        for (i=0; i<16; i++){
+            letters[i].style.animation = "none";
+        }
+        for (i=0; i<5; i++){
+            ele[i].style.animation = "none";
+        }
+    }
+    
+});
+
+//=====================================================================
