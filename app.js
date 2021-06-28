58756238a7215799bfb2900a698604bd3cc5a867
Jadit19 <aditj20@iitk.ac.in>
2021-06-28 19:48:58 +0530

    Add Start Icons

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ var home = document.getElementById('home');
 home.addEventListener('click', () => {
     if (start.style.bottom == "0px"){
         start.style.bottom = "80px";
-        start.style.height = "60%";
-        start.style.width = "50%";
         start.style.zIndex = 4;
+        start.style.opacity = 0.95;
         console.log('Opened Start Menu')
     } else {
         // start.innerHTML = '';
         start.style.bottom = "0px";
-        start.style.height = "0%";
-        start.style.width = "0%";
         start.style.zIndex = -1;
+        start.style.opacity = 0;
         console.log('Closed Start Menu');
     }
+});
+
+var power = document.getElementById('power-off-btn');
+var power_content = document.getElementById('power-content');
+var cross = document.getElementById('cross');
+power.addEventListener('click', () => {
+    power_content.style.opacity = 1;
+    power_content.style.zIndex = 10;
+    power_content.style.cursor = "pointer";
+});
+cross.addEventListener('click', () => {
+    power_content.style.opacity = 0;
+    power_content.style.zIndex = -4;
+    power_content.style.cursor = "default";
 });
